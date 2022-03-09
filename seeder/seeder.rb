8310a3832ec118b72d99890d1b8d5c599af92b17
yastamardika <rattyasta1@gmail.com>
2022-03-10 05:32:08 +0700

    enable seeder

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -4,7 +4,15 @@ pages << {
     headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
     url: "https://www.ikea.co.id/in/page/newItems/",
     vars: {
-      category: "LCD Monitor"
+      category: "Newest Product"
+    },
+    driver:{
+        pre_code: "page.once('load', () => console.log('Page loaded!'));
+        await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);
+      await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);
+      await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);
+      await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);
+      await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);"
     },
     fetch_type: "browser"
 }
