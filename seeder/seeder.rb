d4a4a960d44bd04049e09ad58785c1ddaef0e385
yastamardika <rattyasta1@gmail.com>
2022-03-10 05:12:39 +0700

    add seeder

---
+++ seeder/seeder.rb
@@ -0,0 +1,10 @@
+pages << {
+    page_type: "search",
+    method: "GET",
+    headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+    url: "https://www.ikea.co.id/in/page/newItems/",
+    vars: {
+      category: "LCD Monitor"
+    },
+    fetch_type: "browser"
+}
